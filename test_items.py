50d22faaecda16986e3b587d964193e83a6794db
Max Kalinich <maximk@tradeix.com>
2019-10-15 16:38:42 +0300

    Stepik automation course homework, lesson 3.6, step 9

---
+++ test_items.py
@@ -0,0 +1,14 @@
+import time
+import pytest
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+
+
+def test_single_add_to_basket_button_should_be_present(browser):
+    browser.get(link)
+    add_to_basket_button = WebDriverWait(browser, 5).until(EC.element_to_be_clickable((By.CLASS_NAME, "btn-add-to-basket")))
+    add_to_basket_button_elements = browser.find_elements_by_class_name("btn-add-to-basket")
+    assert len(add_to_basket_button_elements) == 1
